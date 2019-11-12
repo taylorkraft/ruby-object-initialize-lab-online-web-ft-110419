5438d12f5976aed7374a8c1003ba5773dfa31764
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-12 05:46:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  def initialize(dogs_name,breed = "Mutt")
+    @name = dogs_name
+    @breed = breed
+  end
+  
+  def name=(dogs_name, breed = "Mutt")
+    @name = dogs_name
+    @breed = breed("Mutt")
+  end
+  
+  def name 
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+end
